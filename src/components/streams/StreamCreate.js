3d0282640593c587d665f7ceb57ef721375a7d10
luiz-junior <junior.rodrigues0222@gmail.com>
2019-02-04 17:28:09 -0200

    redux-form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,20 +2,42 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
-    renderInput({ input }) {
-        return <input { ...input } />
+    renderInput({ input, label }) {
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        );
+    };
+
+    onSubmit(formValues) {
+
     };
 
     render() {
         return (
             <div>
-                <form>
-                    <Field name="title" component={this.renderInput} />
-                    <Field name="description" component={this.renderInput} />
+                <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+                    <Field name="title" component={this.renderInput} label="Enter Title " />
+                    <Field name="description" component={this.renderInput} label="Enter Description " />
+                    <button className="ui button primary">Submit</button>
                 </form>
             </div>
         )
     }
+};
+
+const validade = (formValues) => {
+    const errors = {};
+
+    if (!formValues.title) 
+        errors.title = 'You must enter a title';
+
+    if (!formValues.description) 
+        errors.description = 'You must enter a description';
+
+    return errors;
 };
 
 export default reduxForm({
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,6 +1,6 @@
 import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
-const  INITIAL_STATE = {
+const INITIAL_STATE = {
     isSignedIn: null,
     userId: null, 
 };
